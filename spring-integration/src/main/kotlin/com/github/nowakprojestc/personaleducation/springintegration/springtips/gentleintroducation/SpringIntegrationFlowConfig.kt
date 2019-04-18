d03e841f29c8f12e2a710ef4aad69b65f009537d
mnowak <mateusz.nowak@intenso.pl>
2019-04-18 15:46:36 +0200

    #132 Add adapter.

--- spring-integration/src/main/kotlin/com/github/nowakprojestc/personaleducation/springintegration/springtips/gentleintroducation/SpringIntegrationFlowConfig.kt
+++ spring-integration/src/main/kotlin/com/github/nowakprojestc/personaleducation/springintegration/springtips/gentleintroducation/SpringIntegrationFlowConfig.kt
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.ImageBanner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.core.env.Environment
 import org.springframework.core.io.FileSystemResource
 import org.springframework.integration.dsl.IntegrationFlow
@@ -14,8 +15,13 @@ import org.springframework.integration.file.dsl.Files
 import org.springframework.integration.ftp.dsl.Ftp
 import org.springframework.integration.ftp.session.DefaultFtpSessionFactory
 import org.springframework.integration.transformer.GenericTransformer
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.MessageBuilder
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.PrintStream
 
+@Profile("gentle-introduction")
 @Configuration
 internal class SpringIntegrationFlowConfig {
 
@@ -33,21 +39,30 @@ internal class SpringIntegrationFlowConfig {
 
     @Bean
     fun files(
-            @Value(value = "\${input-directory}:\${HOME}/Desktop/in") fileDirectory: File,
+            @Value(value = "gentle-introduction.file-directory") fileDirectory: File,
             environment: Environment,
             ftpSessionFactory: DefaultFtpSessionFactory
     ): IntegrationFlow {
 
-        val fileStringGenericTransformer: GenericTransformer<File,String> = GenericTransformer<File, String> {source ->
-            val imageBanner = ImageBanner(FileSystemResource(source))
-            imageBanner.printBanner(environment, javaClass, )
+        val fileStringGenericTransformer: GenericTransformer<File, Message<String>> = GenericTransformer<File, Message<String>> { source ->
+            ByteArrayOutputStream().use { outputStream ->
+                PrintStream(outputStream).use { printStream ->
+                    val imageBanner = ImageBanner(FileSystemResource(source))
+                    imageBanner.printBanner(environment, javaClass, printStream)
+                    MessageBuilder.withPayload(outputStream.toByteArray().toString())
+                            .setHeader(FileHeaders.FILENAME, source.absoluteFile.name)
+                            .build()
+                }
+            }
         }
 
         return IntegrationFlows.from(
                 Files.inboundAdapter(fileDirectory)
                         .autoCreateDirectory(true)
-                        .preventDuplicates(true))
-                .transform(File::class.java,fileStringGenericTransformer)
+                        .preventDuplicates(true)
+                        .patternFilter("*.jpg")
+                )
+                .transform(File::class.java, fileStringGenericTransformer)
                 .handle(
                         Ftp.outboundAdapter(ftpSessionFactory)
                                 .fileNameGenerator { message -> (message.headers[FileHeaders.FILENAME] as String).split("\\.")[0] + ".txt" }
