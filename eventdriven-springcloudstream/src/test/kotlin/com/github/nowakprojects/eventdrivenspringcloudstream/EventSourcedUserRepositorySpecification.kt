b80c15791f383b14a34846a70f56876837e831c9
mnowak <mateusz.nowak@intenso.pl>
2019-04-23 11:24:07 +0200

    #190 Add EventSourcedUserRepositorySpecification.

---
+++ eventdriven-springcloudstream/src/test/kotlin/com/github/nowakprojects/eventdrivenspringcloudstream/EventSourcedUserRepositorySpecification.kt
@@ -0,0 +1,33 @@
+package com.github.nowakprojects.eventdrivenspringcloudstream
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+internal class EventSourcedUserRepositorySpecification : Spek({
+    describe("a in memory user repository") {
+
+        val repository: UserRepository by memoized { EventSourcedUserRepository() }
+
+        it("should be able to save and load user") {
+            val savedUser = User.withNickname("Nickname")
+                    .also {
+                        it.activate()
+                        it.changeNicknameTo("Mateusz")
+                    }.also {
+                        repository.save(it)
+                    }
+
+            val loadedUser = repository.find(savedUser.uuid)
+
+            assertNotNull(loadedUser)
+            assertEquals(loadedUser, savedUser)
+            assertEquals(loadedUser?.getNickname(), "Mateusz")
+            assertTrue(loadedUser?.isActivated()!!)
+        }
+
+    }
+
+})
