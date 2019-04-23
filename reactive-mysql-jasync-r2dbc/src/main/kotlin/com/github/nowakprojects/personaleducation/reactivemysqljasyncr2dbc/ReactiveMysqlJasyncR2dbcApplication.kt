8fefc9e58aec7214cec6d2ce7c7e00d620c6ac2b
MateuszNowak <kontakt.mateusznowak@gmail.com>
2019-04-23 23:03:36 +0200

    #47 Add projects for reactive MySQL

---
+++ reactive-mysql-jasync-r2dbc/src/main/kotlin/com/github/nowakprojects/personaleducation/reactivemysqljasyncr2dbc/ReactiveMysqlJasyncR2dbcApplication.kt
@@ -0,0 +1,11 @@
+package com.github.nowakprojects.personaleducation.reactivemysqljasyncr2dbc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactiveMysqlJasyncR2dbcApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReactiveMysqlJasyncR2dbcApplication>(*args)
+}
---
+++ reactive-mysql-jasync-r2dbc/src/test/kotlin/com/github/nowakprojects/personaleducation/reactivemysqljasyncr2dbc/ReactiveMysqlJasyncR2dbcApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.github.nowakprojects.personaleducation.reactivemysqljasyncr2dbc
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ReactiveMysqlJasyncR2dbcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
