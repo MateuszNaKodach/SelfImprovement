288c9d5ebe2e96f1cc69135318f8ab0eb1e3c934
MateuszNowak <kontakt.mateusznowak@gmail.com>
2019-08-10 21:04:33 +0200

    #1305 Add test dependencies.

--- kotlin-coroutines/build.gradle.kts
+++ kotlin-coroutines/build.gradle.kts
@@ -11,9 +11,25 @@ repositories {
     mavenCentral()
 }
 
+object Versions{
+    val jUnit = "5.3.2"
+    val assertj = "3.12.2"
+    val assertk = "0.17"
+    val spek = "2.0.5"
+}
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx","kotlinx-coroutines-core","1.2.2")
+
+    testCompile("org.jetbrains.kotlin:kotlin-test")
+    testCompile("org.junit.jupiter:junit-jupiter-api:${Versions.jUnit}")
+    testCompile("org.junit.jupiter:junit-jupiter-params:${Versions.jUnit}")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:${Versions.jUnit}")
+    testCompile("org.assertj:assertj-core:${Versions.assertj}")
+    testCompile("org.spekframework.spek2:spek-dsl-jvm:${Versions.spek}")
+    testRuntime("org.spekframework.spek2:spek-runner-junit5:${Versions.spek}")
+    testCompile("com.willowtreeapps.assertk:assertk-jvm:${Versions.assertk}")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ kotlin-coroutines/src/test/kotlin/com/github/nowakprojects/learning/kotlin/pluralsight/usingcoroutines/part2/SimpleCoroutineTest.kt
@@ -0,0 +1,7 @@
+package com.github.nowakprojects.learning.kotlin.pluralsight.usingcoroutines.part2
+
+//https://app.pluralsight.com/player?course=kotlin-using-coroutines&author=kevin-jones&name=439068f6-0dd2-4507-bf07-df53c08d9a2a&clip=3&mode=live
+//testowy001@niepodam.pl niepodam123!
+
+internal class SimpleCoroutineTest {
+}
